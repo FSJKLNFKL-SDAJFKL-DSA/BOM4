4e3f80173e9f4ade8246d541e9f0232f62acff16
TimothyKuijper <t.kuijper12@gmail.com>
2023-06-08 13:08:32 +0200

    mashing things togheter

--- BOM4/Assets/Scripts/SpawnerBehavior.cs
+++ BOM4/Assets/Scripts/SpawnerBehavior.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class SpawnerBehavior : MonoBehaviour
 {
+
+    public GameObject Enemie;
+    public float spawningTime;
     void Start()
-    {
-        
+    { 
+    
     }
+
 
     void Update()
     {
-        
+        spawningTime = Time.time;
+        if (spawningTime > 10) 
+        {
+            var enemieLVL1 = Instantiate(Enemie);
+            spawningTime = 0;
+        }
     }
 }
