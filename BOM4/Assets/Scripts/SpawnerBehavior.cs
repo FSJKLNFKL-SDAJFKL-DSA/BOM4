a818597a711e84f8605a65049e11e654ce37d787
TimothyKuijper <t.kuijper12@gmail.com>
2023-06-13 09:02:08 +0200

    tried spawner things

--- BOM4/Assets/Scripts/SpawnerBehavior.cs
+++ BOM4/Assets/Scripts/SpawnerBehavior.cs
@@ -8,18 +8,13 @@ public class SpawnerBehavior : MonoBehaviour
     public GameObject Enemie;
     public float spawningTime;
     void Start()
-    { 
-    
+    {
+        
     }
 
 
     void Update()
     {
-        spawningTime = Time.time;
-        if (spawningTime > 10) 
-        {
-            var enemieLVL1 = Instantiate(Enemie);
-            spawningTime = 0;
-        }
+
     }
 }
